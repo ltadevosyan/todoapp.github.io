9bb382ae608f5ccf7752b248b2b052e9866e5865
ltadevosyan <99826162+ltadevosyan@users.noreply.github.com>
2022-07-11 23:55:05 +0400

    Add files via upload

---
+++ scriptToDo.js
@@ -0,0 +1,49 @@
+const myNodeList = document.getElementsByTagName('LI');
+for (let i = 0; i < myNodeList.length; i++) {
+    const span = document.createElement("SPAN");
+    const text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    myNodeList[i].appendChild(span);
+}
+
+const close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick =function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+function newElement() {
+    const li = document.createElement("li");
+    const inputValue = document.getElementById("myInput").value;
+    const t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === "") {
+        alert("This line cannot be empty!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    const span = document.createElement("SPAN");
+    const text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            const div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
